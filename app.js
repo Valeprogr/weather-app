bbd79dfe963d267ea16593737dcd1baebe949022
valeviit <valeviit@icloud.com>
2022-11-22 23:09:47 +0100

    add env , add api and make get request

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
+const { response } = require("express");
 const express = require("express");
 const app = express();
 const PORT = 4000;
+const https = require("https");
+const dotenv = require('dotenv').config();
+
 
 app.get("/",(req,res,next)=>{
+const key = process.env.MY_KEY;
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=${key}`;
+
+    https.get(url, (res)=>{
+        console.log(res.statusCode)
+    })
     res.send(`<h1>Hello World!</h1>`);
 })
 
 app.listen(PORT, ()=>{
     console.log(`Server started on PORT ${PORT}`);
 })
+

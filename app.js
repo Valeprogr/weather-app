5dae004f2ab452ec4bab6a628a3a83d6a10eddaa
valeviit <valeviit@icloud.com>
2022-11-26 12:53:17 +0100

    add a post requ + connect html file

--- app.js
+++ app.js
@@ -4,32 +4,40 @@ const app = express();
 const PORT = 4000;
 const https = require("https");
 const dotenv = require('dotenv').config();
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/",(req,res,next)=>{
-const key = process.env.MY_KEY;
-const url = `https://api.openweathermap.org/data/2.5/weather?q=Berlin&appid=${key}&units=metric`;
+app.get("/", (req, res, next) => {
+    res.sendFile(__dirname + "/index.html")
 
-    https.get(url, (response)=>{
+});
+app.post("/", (req, res, next) => {
+    const key = process.env.MY_KEY;
+    const query = req.body.cityName;
+    const unit = "metric";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${key}&units=${unit}`;
+
+    https.get(url, (response) => {
         console.log(res.statusCode);
-        response.on("data", (data)=>{
-          const weatherData = JSON.parse(data)
-          const temp = weatherData.main.temp;
-          const weatherDescription= weatherData.weather[0].description;
-          const icon = weatherData.weather[0].icon;
-          const imageUrl= `http://openweathermap.org/img/wn/${icon}@2x.png`
+        response.on("data", (data) => {
+            const weatherData = JSON.parse(data)
+            const temp = weatherData.main.temp;
+            const weatherDescription = weatherData.weather[0].description;
+            const icon = weatherData.weather[0].icon;
+            const imageUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`
 
-          res.write(`<h1>The temperature in Berlin is ${temp} degrees Celcius </h1>`);
-          res.write(`<p>the weather is currently  ${weatherDescription}</p>`);
-          res.write(`<img src="${imageUrl}"/>`)
-          res.send()
+            res.write(`<h1>The temperature in ${query} is ${temp} degrees Celcius </h1>`);
+            res.write(`<p>the weather is currently  ${weatherDescription}</p>`);
+            res.write(`<img src="${imageUrl}"/>`)
+            res.send()
 
         })
     })
-    
 })
 
-app.listen(PORT, ()=>{
+
+app.listen(PORT, () => {
     console.log(`Server started on PORT ${PORT}`);
 })
 
